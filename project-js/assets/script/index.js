e9f2f974caf754de22b1def9501d18f4a3790306
arezooKheirabadi <a_kheirabadi@yahoo.com>
2019-12-17 17:41:48 +0330

    my-project are added

---
+++ project-js/assets/script/index.js
@@ -0,0 +1,23 @@
+$(function () {
+  // debugger;
+  // $('.example-popover').popover({
+  //   container: 'body'
+  // })
+
+
+
+
+
+  // $('#exampleModal').on('shown.bs.modal', function () {
+  //     $('#myInput').trigger('focus')
+  //   })
+
+  $('#carouselExampleSlidesOnly').carousel({
+    interval:1000
+  });
+
+
+})
+
+
+
---
+++ project-js/index.js
@@ -0,0 +1,6 @@
+console.log(766678);
+
+function aaaa() {
+    alert('3457');
+}
+$('.alert').alert()
